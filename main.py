90a629ebb8f504d9072630d742b060c5aa0fc1d3
leonardotanzi <leo.tanzi@gmail.com>
2022-05-30 17:51:51 +0200

    block layers

--- main.py
+++ main.py
@@ -13,95 +13,70 @@ import torchvision
 import matplotlib.pyplot as plt
 
 
+class UnetConvBlock(nn.Module):
+    def __init__(self, in_filters, out_filters):
+        super(UnetConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_filters, out_channels=out_filters, kernel_size=(3, 3), stride=(1, 1),
+                               padding="same")
+        self.conv2 = nn.Conv2d(in_channels=out_filters, out_channels=out_filters, kernel_size=(3, 3), stride=(1, 1),
+                               padding="same")
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x, maxpool="True"):
+        x = torch.relu(self.conv1(x))
+        x_res = torch.relu(self.conv2(x))
+
+        return (self.pool(x_res), x_res) if maxpool else x_res
+
+
+class UnetDeconvBlock(nn.Module):
+    def __init__(self, in_filters, out_filters):
+        super(UnetDeconvBlock, self).__init__()
+
+        self.upconv = nn.ConvTranspose2d(in_channels=in_filters, out_channels=in_filters, kernel_size=(2, 2), stride=(2, 2))
+        self.conv1 = nn.Conv2d(in_channels=in_filters+out_filters, out_channels=out_filters, kernel_size=(3, 3), stride=(1, 1),
+                               padding="same")
+        self.conv2 = nn.Conv2d(in_channels=out_filters, out_channels=out_filters, kernel_size=(3, 3), stride=(1, 1),
+                               padding="same")
+
+    def forward(self, x, x_res):
+        x = self.upconv(x)
+        x = torch.cat((x, x_res), dim=1)
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        return x
+
+
 class Unet(nn.Module):
     def __init__(self):
         super(Unet, self).__init__()
-        self.conv11 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv12 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding="same")
+        self.convblock1 = UnetConvBlock(3, 16)
+        self.convblock2 = UnetConvBlock(16, 32)
+        self.convblock3 = UnetConvBlock(32, 64)
+        self.convblock4 = UnetConvBlock(64, 128)
+        self.convblock5 = UnetConvBlock(128, 256)
 
-        self.conv21 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv22 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv31 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv32 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv41 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv42 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv51 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv52 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.upconv1 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=(2, 2), stride=(2, 2))
-        self.upconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=(2, 2), stride=(2, 2))
-        self.upconv3 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=(2, 2), stride=(2, 2))
-        self.upconv4 = nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=(2, 2), stride=(2, 2))
-
-        self.conv61 = nn.Conv2d(in_channels=256+128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv62 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv71 = nn.Conv2d(in_channels=128+64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv72 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv81 = nn.Conv2d(in_channels=64+32, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv82 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding="same")
-
-        self.conv91 = nn.Conv2d(in_channels=32+16, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding="same")
-        self.conv92 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding="same")
+        self.deconvblock1 = UnetDeconvBlock(256, 128)
+        self.deconvblock2 = UnetDeconvBlock(128, 64)
+        self.deconvblock3 = UnetDeconvBlock(64, 32)
+        self.deconvblock4 = UnetDeconvBlock(32, 16)
 
         self.convout = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(1, 1), stride=(1, 1))
 
-
     def forward(self, x):
+        x, x1 = self.convblock1(x)
+        x, x2 = self.convblock2(x)
+        x, x3 = self.convblock3(x)
+        x, x4 = self.convblock4(x)
+        x = self.convblock5(x, maxpool=False)
 
-        x = torch.relu(self.conv11(x))
-        x1 = torch.relu(self.conv12(x))
-        x = self.pool(x1)
-
-        x = torch.relu(self.conv21(x))
-        x2 = torch.relu(self.conv22(x))
-        x = self.pool(x2)
-
-        x = torch.relu(self.conv31(x))
-        x3 = torch.relu(self.conv32(x))
-        x = self.pool(x3)
-
-        x = torch.relu(self.conv41(x))
-        x4 = torch.relu(self.conv42(x))
-        x = self.pool(x4)
-
-        x = torch.relu(self.conv51(x))
-        x = torch.relu(self.conv52(x))
-
-        x = self.upconv1(x)
-        x = torch.cat((x, x4), dim=1)
-
-        x = torch.relu(self.conv61(x))
-        x = torch.relu(self.conv62(x))
-
-        x = self.upconv2(x)
-        x = torch.cat((x, x3), dim=1)
-
-        x = torch.relu(self.conv71(x))
-        x = torch.relu(self.conv72(x))
-
-        x = self.upconv3(x)
-        x = torch.cat((x, x2), dim=1)
-
-        x = torch.relu(self.conv81(x))
-        x = torch.relu(self.conv82(x))
-
-        x = self.upconv4(x)
-        x = torch.cat((x, x1), dim=1)
-
-        x = torch.relu(self.conv91(x))
-        x = torch.relu(self.conv92(x))
+        x = self.deconvblock1(x, x4)
+        x = self.deconvblock2(x, x3)
+        x = self.deconvblock3(x, x2)
+        x = self.deconvblock4(x, x1)
 
         x = torch.sigmoid(self.convout(x))
-
-        return x
-
+        return x 
 
 class CustomDataset(Dataset):
     def __init__(self, img_dir, mask_dir):
@@ -134,7 +109,6 @@ def train_val_dataset(dataset, val_split):
     datasets["train"] = Subset(dataset, train_idx)
     datasets["val"] = Subset(dataset, val_idx)
     return datasets
-
 
 
 def plot_img(test_loader):
@@ -175,7 +149,7 @@ if __name__ == "__main__":
     img_dir = "..\\train\\images"
     ann_dir = "..\\train\\masks"
 
-    n_epochs = 1
+    n_epochs = 50
     batch_size = 16
     learning_rate = 0.001
     momentum = 0.99
